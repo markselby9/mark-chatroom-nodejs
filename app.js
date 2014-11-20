39e020e1ccddbca377a770e3a26fb7324a1d3c7b
markselby <markselbyfcy@gmail.com>
2014-11-20 16:03:43 +0800

    add nickname

--- app.js
+++ app.js
@@ -1,44 +1,69 @@
-var express = require('express')
-var app = express()
-var http = require('http')
-var server = http.createServer(app)
-var io = require('socket.io').listen(server)
-var user_num = 0
-var socket_havename = {}
-app.use("/res", express.static(__dirname + '/res'))
+var express = require('express');
+var app = express();
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+var user_num = 0;
+var usernames = {};
+//routing
+app.use("/res", express.static(__dirname + '/res'));
+
 app.get('/', function(req,res){
 	// var filedir = __dirname+'/index.html' 
 	// console.log(filedir)
 	// res.sendFile(filedir)
 	res.sendFile(__dirname + '/index.html')
-})
+});
 io.on('connection', function(socket){
-	console.log('someone connected to the chatroom.')
-	socket.nickname="anonymous"
-	user_num++
+	user_num++;
+	io.sockets.emit('onlinelist-update',{
+		user_num: user_num,
+		socket_dic: usernames
+	});
+	console.log('Current user_num: '+user_num);
 	socket.on('setnickname', function (name){
-		socket.nickname = name
-		socket_havename[name]=socket
+		delete usernames[socket.nickname];
+		socket.nickname = name;
+		usernames[name] = name;
+		io.sockets.emit('onlinelist-update',{
+			user_num: user_num,
+			socket_dic: usernames
+		});
 		socket.broadcast.emit('broadcastmsg',
 		{
 			nickname: 'system',
 			msg: 'welcome new guy '+name+' !'
 		})
-	})
+	});
 	socket.on('sendmessage', function (msg){
-		console.log(msg)
+		console.log(msg);
 		socket.broadcast.emit('broadcastmsg', 
 			{
 				nickname:socket.nickname,
 				msg:msg
 			})
-	})
+	});
 	socket.on('disconnect', function (){
-		console.log('someone left the chatroom.')
-		user_num--
+		user_num--;
+		//socket_havename.removeItem(socket);
+		if (!usernames.hasOwnProperty(socket.nickname)){
+
+		}else{
+			socket.broadcast.emit('broadcastmsg',
+				{
+					nickname: 'system',
+					msg: socket.nickname+' left the chatroom !'
+				});
+			delete usernames[socket.nickname];
+			io.sockets.emit('onlinelist-update',{
+				user_num: user_num,
+				socket_dic: usernames
+			});
+		}
+		console.log('Current user_num: '+user_num);
 	})
-})
+});
 server.listen(3000, function(){
-	console.log('server started on 3000')
+	console.log('server started on 3000');
 	user_num=0
-})
+});
--- res/client.js
+++ res/client.js
@@ -1,35 +1,55 @@
 var socket = io();
-var nickname = 'anonymous';
-$('#send-button').click(function(){
-	var msg = $('input.input').val()
-	if (msg.length==0){
+var nickname = '';
+$('#input-box').keydown(function(e){
+	if(e.keyCode==13){
+		sendmessage()
+	}
+});
+$('#send-button').click(sendmessage);
+$('#save-nickname').click(function(){
+	var nick = $('#input-nickname').val();
+	if (nick.length!=0){
+		nickname=nick;
+		addmessage('your nickname has been changed to '+nickname+'!');
+		socket.emit('setnickname', nickname);
+	}else{
+		addmessage('your nickname remains anonymous!')
+	}
+	$('#nickname-modal').modal('toggle');
+	$('#save-nickname').val('change nickname: '+nick);
+});
+socket.on('onlinelist-update', function(data){
+	var socket_dic = data.socket_dic;
+	var online_num = data.user_num;
+	console.log()
+	$('#online-number').html('('+online_num+' people online)');
+	var keys = Object.keys(socket_dic);
+	$('#online-list').html('');
+	for (var i = 0; i < keys.length; i++){
+		var temp_name = socket_dic[keys[i]];
+		$('#online-list').append('<li class="list-group-item">'+temp_name+'</li>');
+	}
+});
+socket.on('broadcastmsg', function(data){
+	addmessage(data.nickname + ' :>' + data.msg)
+});
+
+function sendmessage(){
+	var msg = $('input.input').val();
+	if (nickname.length==0){
+
+	}else if (msg.length==0){
 
 	}else{
 		addmessage('You ('+nickname+') :> '+msg)
 		socket.emit('sendmessage', $('input.input').val())	
 	}
-	$('input.input').val('')
-	return false // no refresh
-})
-$('#save-nickname').click(function(){
-	var nick = $('#input-nickname').val()
-	if (nick.length!=0){
-		nickname=nick
-		addmessage('your nickname has been changed to '+nickname+'!')
-		socket.emit('setnickname', nickname)
-	}else{
-		addmessage('your nickname remains anonymous!')
-	}
-	$('#nickname-modal').modal('toggle')
-})
-socket.on('broadcastmsg', function(data){
-	addmessage(data.nickname + ' :>' + data.msg)
-})
-
+	$('input.input').val('');
+}
 function addmessage(msg){
 	// alert(msg)
 	var safemsg = $("#x").text(msg).html(); 
-	$('#messages > li:last').append('<li>'+safemsg+'</li>')
-	$('#messages li:odd').addClass("odd")
+	$('#messages > li:last').append('<li>'+safemsg+'</li>');
+	$('#messages li:odd').addClass("odd");
 
 }
